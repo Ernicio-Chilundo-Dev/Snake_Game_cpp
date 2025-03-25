aa09e6405db520a1fd0b6cc565f0848c4e9140cc
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-25 09:12:50 +0200

    criado o jogo da cobra

---
+++ snake.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ncurses.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+//Definicao do tabuleiro
+const int largura = 30;
+const int altura = 15;
+
+struct Ponto{
+    int x,y;
+};
+
+vevtor<Ponto> cobra;
+Ponto comida;
+
+int 
+
