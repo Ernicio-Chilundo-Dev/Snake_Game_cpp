d6c7ce8914149897b5184c84db1e18e93fc53b40
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-25 10:25:20 +0200

    funcao para gerar a comida e para iniciar o jogo creiadas

--- snake.cpp
+++ snake.cpp
@@ -17,5 +17,26 @@ struct Ponto{
 vevtor<Ponto> cobra;
 Ponto comida;
 
-int 
+int direcao = KEY_RIGHT;
+bool jogoRodando = true;
+int pontuacao = 0;
+
+// Funcao para gerar nova comida
+void gerarComida(){
+    comida.x = rand()  %  (largura -2)+1;
+    comida.y = rand() % (altura - 2)+1;
+}
+
+//Funcao para inicializar o jogo
+void iniciarJogo(){
+    initscr();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    timeout(100)
+
+    srand(time(0));
+    cobra = {{largura/2, altura/2}};
+    gerarComida()
+}
 
