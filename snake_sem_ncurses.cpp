5377867042dd475baef69983732a52b93d5b3897
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-26 18:42:21 +0200

    criado funcao setUp

--- snake_sem_ncurses.cpp
+++ snake_sem_ncurses.cpp
@@ -4,8 +4,25 @@
 
 using namespace std;
 
-//Dimensaodo mapa
+//Dimensao do mapa
 const int largura=40, altura=20;
-int x,y,comidaX,comiddaY;
+int x,y,comidaX,comidaY,pontuacao;
 int caudaX[100],caudaY[100];
 int ncauda;
+
+enum Direcao{STOP=0, ESQUERDA,DIREITA,BAIXO,CIMA};
+Direcao dir;
+
+bool gameOver;
+
+void setUp(){
+    gameOver = false;
+    dir = STOP;
+
+    x=largura/2;
+    y=altura/2;
+    comidaX= rand() % largura;
+    comidaY = rand() % altura;
+    pontuacao = 0;
+
+}
