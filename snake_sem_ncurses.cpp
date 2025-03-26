fba07c1ab1ae1b47567fea08ca88302f4fac4cd2
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-26 18:21:30 +0200

    criado o mesmo jogo snake sem usar a biblioteca ncurses.h

---
+++ snake_sem_ncurses.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+//Dimensaodo mapa
+const int largura=40, altura=20;
+int x,y,comidaX,comiddaY;
+int caudaX[100],caudaY[100];
+int ncauda;
